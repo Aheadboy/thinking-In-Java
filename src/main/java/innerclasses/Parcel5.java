1a828c259fc0612e725428916ee67f119d63b856
pure <502964379@qq.com>
2019-12-10 10:18:41 +0800

    内部类，嵌入在外部类的方法中

--- src/main/java/innerclasses/Parcel5.java
+++ src/main/java/innerclasses/Parcel5.java
@@ -1,13 +1,14 @@
 //: innerclasses/Parcel5.java
 package innerclasses; /* Added by Eclipse.py */
 // Nesting a class within a method.
-
+//方法中嵌入内部类。
 public class Parcel5 {
   public Destination destination(String s) {
     class PDestination implements Destination {
       private String label;
       private PDestination(String whereTo) {
         label = whereTo;
+        System.out.println("father is :"+Parcel5.this.getClass().getSimpleName());
       }
       public String readLabel() { return label; }
     }
